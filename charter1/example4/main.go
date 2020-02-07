961ae0b00ce7e1544c165b87afc0d3dfedf510ff
cpChen <305983806@qq.com>
2020-02-07 12:14:09 +0800

    defer

---
+++ charter1/example4/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/*
+defer后面的函数在defer语句所在的函数执行结束的时候前才会被调用
+作用1：类似于 JAVA try...catch...finally 中的 finally
+注意：defer后面必须是函数调用语句或方法调用语句，不能是其他语句，否则编译器会出错。
+*/
+func main() {
+	sample1()
+}
+
+
+func sample1() {
+	for i := 0; i < 5; i++ {
+		// defer 后的多个函数，也会根据声明顺序，倒序执行，输出 4 3 2 1 0
+		defer fmt.Printf("defer：最后输出 %d \n", i)
+	}
+	fmt.Println("do something...")
+}
+
+func sample2() {
+	//file.Open("file.txt")
+	//defer file.Close()
+}
